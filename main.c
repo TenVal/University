4f761e195a594536e723612cd8cb66555cb79408
Ten Valeriya <charagon63@mail.ru>
2020-05-18 15:16:08 +0300

    Add difference

--- main.c
+++ main.c
@@ -2,10 +2,12 @@
 
 int main(int argc, char const *argv[])
 {
-	int i, j, sum;
+	int i, j, sum, dif;
 	scanf("%d%d", &i,&j);
 	sum = i + j;
+	dif = i - j;
 	printf("%d %d", i, j);
 	printf("%d", sum);
+	printf("%d", dif);
 	return 0;
 }
