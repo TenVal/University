58130e5dbd6fe28f2397c4862c301ea97d66ebad
Student Ten Valeriya <charagon63@mail.ru>
2020-05-18 18:52:54 +0300

    Add division

--- main.c
+++ main.c
@@ -1,13 +1,15 @@
-#include <stdlib.h>
+#include <stdio.h>
 
 int main(int argc, char const *argv[])
 {
-	int i, j, sum, dif;
+	int i, j, sum, dif, division;
 	scanf("%d%d", &i,&j);
 	sum = i + j;
 	dif = i - j;
+	division = i / j;
 	printf("%d %d", i, j);
 	printf("%d", sum);
 	printf("%d", dif);
+	printf("%d", division);
 	return 0;
 }
