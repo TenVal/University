a2f893afbcbf375513bfbd97c908b823f77c9148
Student Ten Valeriya <charagon63@mail.ru>
2020-05-18 18:56:13 +0300

    Add multiplication

--- main.c
+++ main.c
@@ -1,13 +1,15 @@
-#include <stdlib.h>
+#include <stdio.h>
 
 int main(int argc, char const *argv[])
 {
-	int i, j, sum, dif;
+	int i, j, sum, dif, mult;
 	scanf("%d%d", &i,&j);
 	sum = i + j;
 	dif = i - j;
+	mult = i * j;
 	printf("%d %d", i, j);
 	printf("%d", sum);
 	printf("%d", dif);
+	printf("%d", mult);
 	return 0;
 }
