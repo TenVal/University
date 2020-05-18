c46dac0fa0fc22f49ed6b4ec5d2a50bc6a4384c6
Student Ten Valeriya <charagon63@mail.ru>
2020-05-18 12:03:37 +0300

    update main.c

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+	int i, j, sum;
+	scanf("%d%d", &i,&j);
+	sum = i + j;
+	printf("%d %d", i, j);
+	printf("%d", sum);
+	return 0;
+}
